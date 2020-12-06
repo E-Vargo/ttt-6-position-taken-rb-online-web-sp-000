f3053a9727a78afee895946c608443dd8f33ebea
Elliott Vargo <vargoelliott@gmail.com>
2020-12-06 23:09:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X", " ", "O", " ", " ", " ", " ", " ", " "]
 
 
 def position_taken?(board, index)
