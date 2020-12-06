acba6f12204072884bc2a73544b3cf727e47af5c
Elliott Vargo <vargoelliott@gmail.com>
2020-12-06 23:12:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,7 +7,7 @@ def position_taken?(board, index)
   if board[index] == ("") || (" ") || nil
     return false
 
-  elsif board[index] == X || O
+  elsif board[index] == "X" || "O"
       true
   end
 end
