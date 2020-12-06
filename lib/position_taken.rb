f9fdb7796ef653c768a8a71c96ba3cbd7e75ee42
Elliott Vargo <vargoelliott@gmail.com>
2020-12-06 22:48:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == ("") || (" ")
     return false
+  elsif board[index] == ("X") || ("O")
+    return true
   end
 end
